0cbae15a818c695eb7c5b471960ce7b47f48dd2f
Breno Cardoso <elisiobreno@gmail.com>
2016-05-09 23:56:25 -0300

    Login com Soap: Quase lá

--- serv_auth/log.php
+++ serv_auth/log.php
@@ -1,13 +1,20 @@
 <?php //usa o cliete soap
-	include('lib/nusoap.php');	
-	
-	$cliente = new nusoap_client('http://localhost/dist-gt/serv_auth/serv_soap.php?wsdl');
-	
-	$username = $_POST['username'];
-	$senha = $_POST['senha'];
+	include('lib/nusoap.php');
 
-	$parametros = array('username'=>$username, 'senha'=>$senha);
-	$resultado = $cliente->call('login', $parametros);
-	
-	//echo utf8_encode($resultado);
+	$cliente = new nusoap_client('http://localhost/dist-gt/serv_auth/serv_soap.php?wsdl');
+	$parametros = array('username'=>$_POST['username'], 'senha'=>$_POST['senha']);
+	$resultado = $cliente->call('logar', $parametros);
+
+	echo "Resultado: ".$resultado;
+
+	/*if ($resultado > 0) {
+		session_start();
+			$_SESSION["username"] = $_POST['username'];
+			$_SESSION["senha"] = $_POST['username'];
+		echo "<center>você foi autenticado com sucesso....</center>";
+		header("Location: indexCliente.php");
+	} else {
+		echo "<center>você não foi autenticado com suecsso....</center><br/>".$resultado;
+		header("Location: ../index.php");
+	}*/
 ?>
--- serv_auth/serv_soap.php
+++ serv_auth/serv_soap.php
@@ -1,29 +1,42 @@
 <?php
 	include('lib/nusoap.php');
-	include('cliente/auth.php');
 	
 	$servidor = new nusoap_server();	
-	$servidor->configureWSDL('urn:Servidor');
-	$servidor->wsdl->schemaTargetNamespace = 'urn:Servidor';	
+	$servidor->configureWSDL('urn:Serv_soap');
+	$servidor->wsdl->schemaTargetNamespace = 'urn:Serv_soap';	
 	
+	//functions
+	function logar($username, $senha) {
+		echo("\n Cheguei Aqui \n");
+
+		error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
+		$conexao = mysql_connect("localhost", "root", "") or die(mysql_error());
+		mysql_select_db("bd_auth") or die(mysql_error());
+
+		$sql = mysql_query("SELECT * FROM cliente WHERE username_cliente = ".$username." and senha_cliente = ".$senha) or die(mysql_error());
+		$row = mysql_num_rows($sql);
+	
+		return $row;
+	}
+	
+	function logoff() {
+		session_start();
+		session_destroy();
+		header("Location: ../../index.php");
+	}
+
 	$servidor->register(
-		'login',
-		array('username'=>'xsd:string',
-				'senha'=>'xsd:string'),
-		array('retorno'=>'xsd:boolean'),
-		'urn:Servidor.exemplo',
-		'urn:Servidor.exmeplo',
-		'rpc',
-		'encoded',
-		'NuSOAP PHP.'
+		'logar',
+		array('username'=>'xsd:string', 'senha'=>'xsd:string'),
+		array('retorno'=>'xsd:integer')
 	);
 
 	/*$servidor->register(
 		'logoff',
 		array(),
 		array(),
-		'urn:Servidor.exemplo',
-		'urn:Servidor.exmeplo',
+		'urn:serv_soap.logoff',
+		'urn:serv_soap.logoff',
 		'rpc',
 		'encoded',
 		'NuSOAP PHP.'
@@ -31,4 +44,5 @@
 	
 	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
 	$servidor->service($HTTP_RAW_POST_DATA);
+
 ?>
