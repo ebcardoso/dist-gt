a99984455afa53578957fb693e608cd49cf8b977
Breno Cardoso <elisiobreno@gmail.com>
2016-05-20 12:17:54 -0300

    Mais CSS

--- serv-auth/banco/criar_conta.php
+++ serv-auth/banco/criar_conta.php
@@ -1,12 +1,33 @@
 <html>
 <head> <title> Criar Conta </title> </head>
 
-<body>
-	<h2> Dados da Conta </h2>
-	<form nome="formlogin "action="criar.php" method="post">
-		<label>Saldo Inicial:</label>
-		<input type="text" name="saldo"/>
-		<input type="submit" name="criar" value="Criar"/>
-	</form>
-</body>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6 ielt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7 ielt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<title>Empréstimo</title>
+	<link rel="stylesheet" type="text/css" href="../style/style.css" />
+	</head>
+
+	<body>
+		<div class="container">
+			<section id="content">
+				<form nome="formlogin "action="criar.php" method="post">
+					<h1>Criar Conta</h1>
+
+					<div>
+						<input type="text" name="saldo" required="" id="username" />
+					</div>
+					<div>
+						<input type="submit" value="Confirmar" />
+						<?php //<a href="#">Lost your password?</a> ?>
+						<a href="index.php">Voltar</a>
+					</div>
+				</form>
+			</section><!-- content -->
+		</div><!-- container -->
+	</body>
 </html>
--- serv-auth/banco/index.php
+++ serv-auth/banco/index.php
@@ -1,33 +1,56 @@
-<html>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6 ielt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7 ielt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
 	<head>
-		<title>
-			Página de Login
-		</title>
+	<meta charset="utf-8">
+	<title>Banco</title>
+	<link rel="stylesheet" type="text/css" href="../style/style.css" />
 	</head>
-	
-	<body>
-		<h1> Index Banco </h1>
 
-		<?php
-			include('../../nusoap_lib/nusoap.php');	
-			$cliente = new nusoap_client('http://localhost/dist-gt/banco/servidor.php?wsdl');
+		<body>
+		<div class="container">
+			<section id="content">
+				<h1>Banco</h1>
 
-			session_start();
-			$username = $_SESSION['username'];
-			$parametros = array('user_titular'=>$username);
-			$res = $cliente->call('ver_se_tem_conta', $parametros);
+				<?php
+					include('../../nusoap_lib/nusoap.php');	
+					$cliente = new nusoap_client('http://localhost/dist-gt/banco/servidor.php?wsdl');
 
-			if ($res > 0) {//quando a pessoa tem conta, mostra o saldo
-				$res = $cliente->call('pegar_saldo', $parametros);
-				echo "<p> Titular:".$username."<br/>Saldo: R$ ".number_format($res,2,',','.')."</p>";
-				echo "<p> <a href='cancelar_conta.php?conta=".$username."'> Destruir Conta </a> </p>";
-				echo "<p> <a href='fazer_emprestimo.php'> Empréstimo </a>";
-				echo "<p> <a href='extrato_da_conta.php'> Extrato </a>";
-			} else {
-				echo "<p>Você ainda não possui uma conta</p>";
-				echo "<p> <a href='criar_conta.php'> Criar Conta </a> </p>";
-			}
-			echo "<p> <a href='../indexCliente.php'> Voltar </a>";
-		?>
+					session_start();
+					$username = $_SESSION['username'];
+					$parametros = array('user_titular'=>$username);
+					$res = $cliente->call('ver_se_tem_conta', $parametros);
+
+					if ($res > 0) {//quando a pessoa tem conta, mostra o saldo
+						$res = $cliente->call('pegar_saldo', $parametros);
+						echo "<p> Titular:".$username."<br/>Saldo: R$ ".number_format($res,2,',','.')."</p> <br/>";
+					?>
+					
+					<div class="button">
+					<?php
+						echo "<p> <a href='extrato_da_conta.php'> Extrato </a>";
+					?>
+					</div>
+					<div class="button">
+					<?php
+						echo "<a href='fazer_emprestimo.php'> Empréstimo </a>";
+					?>
+					</div>
+					<div class="button">
+					<?php
+						echo "<a href='cancelar_conta.php?conta=".$username."'> Destruir Conta </a>";
+					?>
+					</div>
+					<?php
+					} else {
+						echo "<p>Você ainda não possui uma conta </p> <br/>";
+						echo "<div class=\"button\"> <a href='criar_conta.php'> Criar Conta </a> </div>";
+					}
+					echo "<div class=\"button\"> <a href='../indexCliente.php'> Voltar </div> <br/>";
+				?>
+			</section><!-- content -->
+		</div><!-- container -->
 	</body>
 </html>
